f83812d4a6c95dbdd9d5c4dcbc1ed403744f8037
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-22 21:31:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,8 @@ def fizzbuzz(int)
     "Fizz"
   else int % 5 == 0
     "Buzz"
-  elsif (int % 3 == 0) && (int % 5 == 0)
+  else (int % 3 == 0) && (int % 5 == 0)
     "FizzBuzz"
+  elsif (int !% 3 == 0) || (int !% 5 == 0)
   end
 end
