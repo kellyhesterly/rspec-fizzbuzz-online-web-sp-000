12eb3bb2e3becb631f66d3c4b8d080f914f8106e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-22 21:42:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 #I want whatever number is presented to be called int
 def fizzbuzz(int)
-  if int % 3 == 0
+  if (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  else (int % 3 == 0) && (int % 5 == 0)
-    "FizzBuzz"
   #else !(int % 3 == 0) || !(int % 5 == 0)
   #  nil
   end
