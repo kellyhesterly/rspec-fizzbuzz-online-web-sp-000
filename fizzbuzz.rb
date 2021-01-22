b5f8d3c99f4abdd909bc63f3c33a4cfc6a74343b
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-22 21:07:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,27 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+#I want whatever number is presented to be called int
+def fizzbuzz(int)
+  if (int % 3 == 0) && (int % 5 == 0)
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+  "Buzz"
+else int
 end
-
-def fizzbuzz(number)
-  return "FizzBuzz" if is_divisible_by_fifteen?(number)
-  return "Buzz" if is_divisible_by_five?(number)
-  return "Fizz" if is_divisible_by_three?(number)
-  number
-end
-
-def is_divisible_by_three?(number)
-  is_divisible_by(number, 3)
-end
-
-def is_divisible_by_five?(number)
-  is_divisible_by(number, 5)
-end
-
-def is_divisible_by_fifteen?(number)
-  is_divisible_by(number, 15)
-end
-
-def is_divisible_by(number, divisor)
-  number % divisor == 0
 end
